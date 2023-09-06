ebc75a31a6be2f33b92453639300067771195c7f
Roberto Ayres Pimenta <87720894+robertoApimenta@users.noreply.github.com>
2023-09-06 02:47:05 -0300

    readme.md

--- api/src/models/connection.ts
+++ api/src/models/connection.ts
@@ -1,11 +1,16 @@
 import mysql, { Pool } from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT, 10) : 33060;
 
 const pool: Pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  port: 33060,
-  database: 'shopper',
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  port,
+  database: process.env.MYSQL_DATABASE || 'shopper',
 });
 
 export default pool;
